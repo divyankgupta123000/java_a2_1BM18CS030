e9f316010499f695854024333e7e4232a3bc004d
divyankgupta123000 <53806810+divyankgupta123000@users.noreply.github.com>
2019-08-31 20:14:59 +0530

    Update PointDemo.java

--- PointDemo.java
+++ PointDemo.java
@@ -1,47 +1,43 @@
 import java.util.*;
-
-class Point
-{
-  double x;
-  double y;
-  
-  Point()
-  { 
-    x=0;
-    y=0;
-  }
-
-  Point(int a,int b)
-  {
-    
-  }
-
+class Point {
+    private int x,y;
+    Point()
+    {x=0;y=0;}
+    Point(int x,int y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+    Point( Point ob)
+    {
+        x=ob.x;
+        y=ob.y;
+    }
+    double calculate(Point ob)
+    {
+        return (Math.sqrt(Math.pow((x-ob.x),2)+Math.pow((y-ob.y),2)));
+    }
 }
-
-
 class PointDemo
 {
- 
- public static void main(String args[])
- {
-    Scanner sc=new Scanner(System.in);
-    
-    System.out.println("Enter total no. of Points: ");
-    int n=sc.nextInt();
-    Point p[]=new Point[n];
-    for(int i=0;i<n;i++)
+    public static void main(String args[])
     {
-      p[i]=new Point();    
+       Scanner sc=new Scanner(System.in);
+       Point p1=new Point(1,2);
+       Point p2=new Point(p1);
+       System.out.println("distance betwwen p1 and p2 is");
+       double d=p1.calculate(p2);
+       System.out.println(d);
+       System.out.println("Enter 2 co-ordinates");
+       int x=sc.nextInt();
+       int y=sc.nextInt();
+       int x1=sc.nextInt();
+       int y1=sc.nextInt();
+       Point p3=new Point(x,y);
+       Point p4=new Point(x1,y1);
+       double d1=p3.calculate(p4);
+       System.out.println(d1+" is the distance");
     }
-
-    for(int i=0;i<n;i++)
-    {
-     System.out.println("Enter Abscissa: ");
-     x=sc.nextDouble();
-     System.out.println("Enter Ordinate: ");
-     y=sc.nextDouble();
-    }
- }
 }
 
 
